c079007f649938d2aeeb71bd1a191e3f7d4ac219
Patrick Amaro <amaro314.patrick@gmail.com>
2022-10-01 14:57:36 -0700

    Reformat part 1

--- src/com/example/simplechess/Main.java
+++ src/com/example/simplechess/Main.java
@@ -14,22 +14,22 @@ public class Main {
 	public static Scanner scanner;
 	
 	public static void main(String[] args) {
-    	System.out.println("Hello, World!");
-        board = new GameBoard();
-        gameStatus = true;
-        board.setUpClassicBoard();
-        board.printBoard();
-        Player currentPlayer;
-        turn = true;
-        while(gameStatus) {
-        	scanner = new Scanner(System.in); 
-        	currentPlayer = turn ? player1: player2;
-            System.out.println("Select move to do " + currentPlayer.getName() +",");
-            printOptions();
-            String command = scanner.nextLine(); 
-            switch(command) {
-	            case "1": 
-	            	board.printBoard();
+		System.out.println("Hello, World!");
+		board = new GameBoard();
+		gameStatus = true;
+		board.setUpClassicBoard();
+		board.printBoard();
+		Player currentPlayer;
+		turn = true;
+		while(gameStatus) {
+			scanner = new Scanner(System.in);
+			currentPlayer = turn ? player1: player2;
+			System.out.println("Select move to do " + currentPlayer.getName() +",");
+			printOptions();
+			String command = scanner.nextLine();
+			switch(command) {
+				case "1": 
+					board.printBoard();
 	            	System.out.println("Decide next move");
 	            	System.out.println(board.getSpaceAt(0, 0));
 	            	turn = !turn;
@@ -49,7 +49,7 @@ public class Main {
             }
         }
         System.out.println("Game ended");
-    }
+	}
 	
 	public static void printOptions()
 	{
