9aed2cdf24f76c74c291490ff465083e699510e6
Patrick Amaro <amaro314.patrick@gmail.com>
2022-10-02 10:20:29 -0700

    Update main.java logic

--- src/com/example/simplechess/Main.java
+++ src/com/example/simplechess/Main.java
@@ -2,13 +2,16 @@ package com.example.simplechess;
 
 import com.example.simplechess.model.game.Player;
 import com.example.simplechess.model.game.GameBoard;
+import com.example.simplechess.model.game.GameStatus;
 
 import java.util.Scanner;
 
 public class Main {
-	public static boolean gameStatus = false;
+	public static GameStatus gameStatus = GameStatus.ACTIVE;
 	public static boolean turn = true;
 	static GameBoard board;
+	public static Player currentPlayer;
+	public static Player winner;
 	public static Player player1 = new Player("Player 1");
 	public static Player player2 = new Player("Player 2");
 	public static Scanner scanner;
@@ -16,12 +19,10 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
 		board = new GameBoard();
-		gameStatus = true;
 		board.setUpClassicBoard();
 		board.printBoard();
-		Player currentPlayer;
 		turn = true;
-		while(gameStatus) {
+		while(gameStatus == GameStatus.ACTIVE) {
 			scanner = new Scanner(System.in);
 			currentPlayer = turn ? player1: player2;
 			System.out.println("Select move to do " + currentPlayer.getName() +",");
@@ -32,7 +33,7 @@ public class Main {
 					board.printBoard();
 					System.out.println("Decide next move");
 					System.out.println(board.getSpaceAt(0, 0));
-					turn = !turn;
+					switchTurn();
 					break;
 				case "2":
 					printHelp();
@@ -41,7 +42,7 @@ public class Main {
 					board.printBoard();
 					break;
 				case "4": 
-					gameStatus = false;
+					quitGame();
 					break;
 				default: 
 					System.out.println("Do not understand command");
@@ -51,16 +52,30 @@ public class Main {
 		System.out.println("Game ended");
 	}
 	
-	public static void printOptions()
-	{
+	public static void switchTurn() {
+		turn = !turn;
+	}
+
+	public static void quitGame() {
+		gameStatus = GameStatus.FORFEIT;
+		setUpWinner(false);
+		System.out.println(currentPlayer.getName() + " has quit");
+		System.out.println(winner.getName() + " has won");
+	}
+
+	public static void printOptions() {
 		System.out.println("1. Make a move");
 		System.out.println("2. Help");
 		System.out.println("3. Show board");
 		System.out.println("4. Quit");
 	}
-	
-	public static void printHelp()
-	{
+
+	public static void printHelp() {
 		System.out.println("To play chess, you need to know that pieces have different movesets.");
+	}
+
+	public static void setUpWinner(boolean didCurrentPlayerWin) {
+		winner = didCurrentPlayerWin ? currentPlayer
+			: currentPlayer == player1 ? player2 : player1;
 	}
 }
